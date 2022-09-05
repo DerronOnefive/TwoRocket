8b6b5107a9c722a52cb040660d12a3a7e277a10d
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2022-09-05 19:45:33 -0400

    Create TwoRockets.java

---
+++ TwoRockets.java
@@ -0,0 +1,26 @@
+/**
+ * @class: TwoRockets.java
+ * @author: Derron Pierre
+ * @date: August 30, 2022
+ * @version: 1.0
+ * description: this program will display two coded rockets side by side.
+ */
+
+public class TwoRockets {
+    public static void main(String [] args){
+        System.out.println("  /  \\     /  \\");
+        System.out.println(" /    \\   /    \\");
+        System.out.println("/      \\ /      \\");
+        System.out.println("+------+ +------+");
+        System.out.println("|      | |      |");
+        System.out.println("|      | |      |");
+        System.out.println("+------+ +------+");
+        System.out.println("|United| |United|");
+        System.out.println("|States| |States|");
+        System.out.println("+------+ +------+");
+        System.out.println("   /\\     /\\");
+        System.out.println("  /  \\   /  \\");
+        System.out.println(" /    \\ /    \\");
+
+    }
+}
